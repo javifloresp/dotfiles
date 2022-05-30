7ad3c23a31945861abd831d796152b89f58e795d
Javier <javier@retrypay.com>
2022-05-30 02:15:21 -0500

    update nvim [nvchad]

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,4 +1,5 @@
 -- Just an exampln, supposed to be placed in /lua/custom/
+--
 local M = {}
 
 local userPlugins = require "custom.plugins" -- path to table
@@ -161,10 +162,10 @@ M.plugins = {
 -- example of changing theme:
 
 M.ui = {
-  theme           = "expresso",
-  color           = "custom.themes.expresso",
-  theme_toggle    = { "solarized", "expresso" },
-  -- theme           = "solarized",
+  -- theme           = "expresso",
+  -- color           = "custom.themes.expresso",
+  theme_toggle    = { "solarized", "kanagawa" },
+  theme           = "kanagawa",
   transparency    = true,
   italic_comments = true,
 }
@@ -177,29 +178,29 @@ M.options = {
     vim.opt.relativenumber = true -- Its better if you use motions like 10j or 5yk
     vim.opt.hlsearch       = true -- Highlight search results
     vim.g.autosave         = true
-    vim.opt.softtabstop    = 4 -- On insert use 4 spaces for tab
-    vim.opt.tabstop        = 4 -- Tab size of 4 spaces
+    vim.opt.softtabstop    = 2 -- On insert use 4 spaces for tab
+    vim.opt.tabstop        = 2 -- Tab size of 4 spaces
+    vim.opt.showmatch      = true -- Highlights the matching parenthesis
+    vim.opt.ignorecase     = true -- Search ignoring case
+    vim.opt.smartcase      = true -- Do not ignore case if the search patter has uppercase
+    vim.opt.splitright     = true -- New vert splits are on the right
+    vim.opt.splitbelow     = true -- New horizontal splits, like `:help`, are on the bottom window
+    vim.opt.shiftwidth     = 0 -- Number of spaces to use for each step of (auto)indent
+    vim.opt.expandtab      = true -- Use appropriate number of spaces (no so good for PHP but we can fix this in ft)
+    vim.opt.wrap           = false -- Wrapping sucks (except on markdown)
+    vim.opt.swapfile       = false -- Do not leave any backup files
+    vim.opt.mouse          = "i" -- Enable mouse on insert mode
     -- vim.opt.showmatch      = true -- Highlights the matching parenthesis
-    -- vim.opt.ignorecase     = true -- Search ignoring case
-    -- vim.opt.smartcase      = true -- Do not ignore case if the search patter has uppercase
-    -- vim.opt.splitright     = true -- New vert splits are on the right
-    -- vim.opt.splitbelow     = true -- New horizontal splits, like `:help`, are on the bottom window
-    -- vim.opt.shiftwidth     = 0 -- Number of spaces to use for each step of (auto)indent
-    -- vim.opt.expandtab      = true -- Use appropriate number of spaces (no so good for PHP but we can fix this in ft)
-    -- vim.opt.wrap           = false -- Wrapping sucks (except on markdown)
-    -- vim.opt.swapfile       = false -- Do not leave any backup files
-    -- vim.opt.mouse          = "i" -- Enable mouse on insert mode
-    -- -- vim.opt.showmatch      = true -- Highlights the matching parenthesis
-    -- -- vim.opt.termguicolors  = true -- Required for some themes
-    -- vim.opt.cursorline     = true -- Highlight the current cursor line (Can slow the UI)
-    -- vim.opt.signcolumn     = "yes" -- Always show the signcolumn, otherwise it would shift the text
-    -- vim.opt.hidden         = true -- Allow multple buffers
-    -- vim.opt.completeopt    = { "menu", "menuone", "noselect", "noinsert" } -- Let the user decide about the autocomplete
-    -- vim.opt.showmode       = false -- Remove the -- INSERT -- message at the bottom
-    -- vim.opt.updatetime     = 750 -- I have a modern machine. No need to wait that long
-    -- vim.opt.encoding       = "utf-8" -- Just in case
-    -- vim.opt.cmdheight      = 2 -- Shows better messages
-    -- vim.opt.relativenumber = true
+    -- vim.opt.termguicolors  = true -- Required for some themes
+    vim.opt.cursorline     = true -- Highlight the current cursor line (Can slow the UI)
+    vim.opt.signcolumn     = "yes" -- Always show the signcolumn, otherwise it would shift the text
+    vim.opt.hidden         = true -- Allow multple buffers
+    vim.opt.completeopt    = { "menu", "menuone", "noselect", "noinsert" } -- Let the user decide about the autocomplete
+    vim.opt.showmode       = false -- Remove the -- INSERT -- message at the bottom
+    vim.opt.updatetime     = 750 -- I have a modern machine. No need to wait that long
+    vim.opt.encoding       = "utf-8" -- Just in case
+    vim.opt.cmdheight      = 2 -- Shows better messages
+    vim.opt.relativenumber = true
   end,
 }
 
